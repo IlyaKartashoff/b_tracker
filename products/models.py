4b7c00c1dba864f1a5d4e01a3b9e91c0390d486c
herrk <herrkarstone@gmail.com>
2024-07-24 19:44:27 +0300

    1 migration commit

--- products/models.py
+++ products/models.py
@@ -1,38 +1,39 @@
 
 from django.db import models
 
-class Group_of_products(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+# class Group_of_products(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class Product(models.Model):
 
-    LENGTH_CHOICES = [
-        (20, '20 cm'),
-        (30, '30 cm'),
-        (40, '40 cm'),
-        (50, '50 cm'),
-        (60, '60 cm'),
-        (70, '70 cm'),
-        (80, '80 cm'),
-        (100, '100 cm'),
-        (110, '110 cm'),
-        (120, '120 cm'),
-        (130, '130 cm'),
-        (140, '140 cm'),
-        (150, '150 cm'),
-    ]
+    # LENGTH_CHOICES = [
+    #     (20, '20 cm'),
+    #     (30, '30 cm'),
+    #     (40, '40 cm'),
+    #     (50, '50 cm'),
+    #     (60, '60 cm'),
+    #     (70, '70 cm'),
+    #     (80, '80 cm'),
+    #     (100, '100 cm'),
+    #     (110, '110 cm'),
+    #     (120, '120 cm'),
+    #     (130, '130 cm'),
+    #     (140, '140 cm'),
+    #     (150, '150 cm'),
+    # ]
     
     name = models.CharField(max_length=100, unique=True)
-    colour = models.CharField(max_length=100)
-    size = models.IntegerField(choices=LENGTH_CHOICES)
-    quantity = models.PositiveIntegerField()
-    group = models.ForeignKey(to=Group_of_products(), related_name='products', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_photos/')
-    purchase_prise = models.DecimalField(default=0.0, max_digits=7, decimal_places=2)
-    sale_prise = models.DecimalField(default=0.0, max_digits=7, decimal_places=2)
+    # colour = models.CharField(max_length=100, blank=True, null=True)
+    # size = models.IntegerField(choices=LENGTH_CHOICES)
+    # quantity = models.PositiveIntegerField(blank=True, null=True)
+    # group = models.ForeignKey(to=Group_of_products(), related_name='products',
+    #                            on_delete=models.CASCADE, blank=True, null=True)
+    # image = models.ImageField(upload_to='product_photos/', blank=True)
+    # purchase_prise = models.DecimalField(default=0.0, max_digits=7, decimal_places=2, blank=True, null=True)
+    # sale_prise = models.DecimalField(default=0.0, max_digits=7, decimal_places=2, blank=True, null=True)
 
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+from django.views.generic import TemplateView, ListView
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/index.html'
+    context_object_name = 'products'
